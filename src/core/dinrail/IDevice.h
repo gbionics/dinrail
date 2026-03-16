303be9c1537138fceb1b356bbc56a386d10393e2
Silvio Traversaro <silvio.traversaro@gbionics.ai>
2026-03-16 23:15:58 +0100

    Add IDevice interface using Parameters

---
+++ src/core/dinrail/IDevice.h
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: 2006-2021 Istituto Italiano di Tecnologia (IIT)
+// SPDX-FileCopyrightText: Generative Bionics S.R.L.
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef DINRAIL_IDEVICE_H
+#define DINRAIL_IDEVICE_H
+
+#include <dinrail/Parameters.h>
+
+namespace dinrail
+{
+
+/**
+ * @brief Interface implemented by all dinrail devices.
+ *
+ * This is the interface for any class that is a dinrail device.
+ */
+class IDevice
+{
+public:
+    /**
+     * @brief Virtual destructor.
+     */
+    virtual ~IDevice() = default;
+
+    /**
+     * Open the DeviceDriver.
+     * @param config is a list of parameters for the device.
+     * @return true/false upon success/failure
+     */
+    virtual bool open(const Parameters& config) = 0;
+
+    /**
+     * Close the DeviceDriver.
+     * @return true/false on success/failure.
+     */
+    virtual bool close() = 0;
+};
+
+} // namespace dinrail
+
+#endif // DINRAIL_IDEVICE_H
